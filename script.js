b2cec6095a8287bf9e6c710055c24d1624602197
Damini Kesharkar <daminisk272@gmail.com>
2023-10-13 00:51:30 +0530

    Added crudcrud backend

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {  
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.5.1/+esm'
+document.addEventListener('DOMContentLoaded', function() { 
+       
+    //post data into crudcrud
+    async function postData(data){
+        try {
+            let response = await axios.post('https://crudcrud.com/api/c50c9920afd14c1a9c2af15377ec5dbf/notes',{note:data});
+            console.log(response.data);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function getSingleEntry(note){
+        try {
+            let response = await axios.get('https://crudcrud.com/api/c50c9920afd14c1a9c2af15377ec5dbf/notes');
+            const matchingId = response.data.find(item => item.note === note);
+            return matchingId._id;
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    //delete from crudcrud
+    async function deleteData(note){
+        let id = await getSingleEntry(note);
+        try {
+            let response = await axios.delete(`https://crudcrud.com/api/c50c9920afd14c1a9c2af15377ec5dbf/notes/${id}`);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    //edit(put)
+    async function editData(id,newNote){
+
+        try {
+            let response = await axios.patch(`https://crudcrud.com/api/c50c9920afd14c1a9c2af15377ec5dbf/notes/${id}`,{note:newNote})
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     //access elements
     const addNotesBtn = document.getElementById('add-btn');
     const addNoteInput = document.getElementById('add-task');
 
     //click event listner on + button
-    addNotesBtn.addEventListener("click", ()=>{
+    addNotesBtn.addEventListener("click",async ()=>{
         let noteAdded = addNoteInput.value;
         if (noteAdded==''){
             alert('No task is added ')
         }else{
+            await postData(noteAdded);
             addNote(noteAdded);
             addNoteInput.value = '';
         }        
@@ -50,17 +96,19 @@ document.addEventListener('DOMContentLoaded', function() {
     let parentDiv = document.getElementById('new-tasks');
     parentDiv.addEventListener("click",modifyNote)
 
-    function modifyNote(e){
+    async function modifyNote(e){
         if(e.target.class == 'deletenote'){
 
             const div = e.target.parentElement;
             let value = div.firstChild.value;
+            await deleteData(value);
             parentDiv.removeChild(div);
 
         }
         if(e.target.class == 'editnote'){
-            console.log('edit')
             const div = e.target.parentElement;
+
+            let id = await getSingleEntry(div.firstChild.value);
 
             if(div.firstChild.disabled){
                 div.firstChild.disabled = false;
@@ -74,15 +122,32 @@ document.addEventListener('DOMContentLoaded', function() {
             confirmBtn.appendChild(document.createTextNode("confirm edit"))
             innerDiv.appendChild(confirmBtn);
 
-            confirmBtn.addEventListener("click",()=>{
+            confirmBtn.addEventListener("click",async()=>{
                 div.firstChild.disabled = true;
+                await editData(id,div.firstChild.value);
                 innerDiv.removeChild(confirmBtn);
             })
         }           
     }
 
+    //get all saved notes from crudcrud backend
+    async function displaySavedNotes(){
+        try {
+            let response = await axios.get('https://crudcrud.com/api/c50c9920afd14c1a9c2af15377ec5dbf/notes');
+            let length = Object.keys(response.data).length;
+            
+            for(let i=0;i<length;i++){
+                let noteData = response.data[i].note;
+                addNote(noteData);
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     window.addEventListener("load",()=>{
-        // displaySavedNotes();
+        displaySavedNotes();
     })
 })
 
